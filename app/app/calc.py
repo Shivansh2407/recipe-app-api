97b3e650ecc7742c79565eca17f43016631f321d
Shivansh2407 <shivanshsrivastava2000@gmail.com>
2019-12-20 17:52:29 +0530

    Fixed Linting Issues

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,7 +1,8 @@
-def add(x,y):
+def add(x, y):
     """Add two numbers together"""
     return x+y
 
-def subtract(x,y):
+
+def subtract(x, y):
     """Subtract Two numbers together"""
     return y-x
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,12 +1,13 @@
 from django.test import TestCase
 
-from app.calc import add , subtract
+from app.calc import add, subtract
+
 
 class CalcTests(TestCase):
     def test_add_numbers(self):
         """Test That two numbers are added together"""
-        self.assertEqual(add(3,8),11)
+        self.assertEqual(add(3, 8), 11)
 
     def test_subtract_numbers(self):
         """Test That two numbers are subtracted together"""
-        self.assertEqual(subtract(5,11) , 6)
+        self.assertEqual(subtract(5, 11), 6)
